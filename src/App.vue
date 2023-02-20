947893b2566f574c11d552bac25cdfe15826c413
90houlaoheshang <907333918@qq.com>
2023-02-20 14:37:49 +0800

    router-view

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,11 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import Demo from "./components/demo.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo"/>
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo"/>
-    </a>
-  </div>
-  <h1 id="myh1">新加的标签</h1>
-  <HelloWorld msg="Vite + Vue"/>
-  <demo/>
+  <router-view/>
 </template>
 
 <style scoped>
-#myh1 {
-  color: crimson;
-}
 
 .logo {
   height: 6em;
