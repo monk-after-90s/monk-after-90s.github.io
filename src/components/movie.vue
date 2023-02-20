75170f4b31645b74afbe2dde14e63ef2132eed67
90houlaoheshang <907333918@qq.com>
2023-02-20 15:41:44 +0800

    只有一个导航

--- src/components/movie.vue
+++ src/components/movie.vue
@@ -3,39 +3,10 @@
 </script>
 
 <template>
-  <div class="outer">
-    <div class="inner">
-      <el-menu
-          class="el-menu-demo"
-          mode="horizontal">
-        <el-menu-item index="1">
-          <router-link :to="{name:'Index'}">首页</router-link>
-        </el-menu-item>
-        <el-menu-item index="2">
-          <router-link :to="{name:'Tv'}">电视剧</router-link>
-        </el-menu-item>
-        <el-menu-item index="3">
-          <router-link :to="{name:'Movie'}">电影</router-link>
-        </el-menu-item>
-        <el-menu-item index="4">
-          <router-link :to="{name:'Sport'}">体育</router-link>
-        </el-menu-item>
-      </el-menu>
-      <div class="content">电影</div>
-    </div>
-  </div>
+  <div class="content">电影</div>
 </template>
 
 <style scoped>
-.outer {
-  width: 100%;
-}
-
-.inner {
-  width: 100%;
-  margin: 0 auto;
-}
-
 .content {
   width: auto;
   height: 500px;
