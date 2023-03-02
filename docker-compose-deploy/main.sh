b39597d7874c37f615de0e22735dcc71059abec4
90houlaoheshang <907333918@qq.com>
2023-03-02 15:02:05 +0800

    docker compose 配置

---
+++ docker-compose-deploy/main.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# 发生错误时终止
+set -e
+
+# 构建
+npm run build
+
+# 进入构建文件夹
+cd dist
+
+# 如果你要部署到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+
+docker compose build
