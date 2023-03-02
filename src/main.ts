9b2d6e95f34845f58516d05230485cc895301ee6
90houlaoheshang <907333918@qq.com>
2023-03-02 16:06:37 +0800

    Revert "服务器打包测试"
    This reverts commit 0c2abe93bf5776cb9bd25b52e571902b19e076fa.

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,26 @@
 import {createApp} from 'vue'
-// import './style.css'
+import './style.css'
 import App from './App.vue'
-// import ElementPlus from 'element-plus'
-// import 'element-plus/dist/index.css'
-// import router from "./router"
-// import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-// import store from "./store"
-// import api from './api'
-// import {createPinia} from 'pinia'
-// import {useJWT} from "./pinia-store"
-// // @ts-ignore
-// import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import router from "./router"
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import store from "./store"
+import api from './api'
+import {createPinia} from 'pinia'
+import {useJWT} from "./pinia-store"
+// @ts-ignore
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
 const app = createApp(App)
-// app.config.globalProperties.api = api
-// for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-//     app.component(key, component)
-// }
-//
-// app.use(ElementPlus, {locale: zhCn}).use(router).use(store).use(createPinia())
-//
-// const jwtStore = useJWT()
-// app.config.globalProperties.jwtStore = jwtStore
+app.config.globalProperties.api = api
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+
+app.use(ElementPlus, {locale: zhCn}).use(router).use(store).use(createPinia())
+
+const jwtStore = useJWT()
+app.config.globalProperties.jwtStore = jwtStore
 
 app.mount('#app')
